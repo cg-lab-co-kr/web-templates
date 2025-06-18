37485203101ce0c7363ef36b504662c3e0c7160c
cglab <cg-lab@naver.com>
2025-06-18 15:13:34 +0900

    Add files via upload

---
+++ hk-kolmar/js/script.js
@@ -0,0 +1,324 @@
+/* 공통!!!---------------------------------------------- */
+
+// GNB
+const $header = $("header");
+const $menu = $(".gnb > li");
+const $submenu = $(".submenu");
+const duration = 300;
+
+$menu.on("mouseenter", function () {
+  $(this).addClass("on");
+  $header.addClass("active");
+  $submenu.stop().slideDown(duration);
+});
+
+$menu.on("mouseleave", function () {
+  $(this).removeClass("on");
+  $header.removeClass("active");
+  $submenu.stop().slideUp(duration);
+});
+
+//모바일 버전의 GNB!!
+// 모바일 메뉴 기능
+const $btnMenu = $(".btn-menu");
+const $btnClose = $(".btn-close");
+const $mobileMenu = $(".mobile-menu");
+const $mobileGnb = $(".mobile-gnb > li > a");
+
+$btnMenu.on("click", function () {
+  $mobileMenu.addClass("on");
+  $("body").css("overflow", "hidden"); // 스크롤 방지
+});
+
+$btnClose.on("click", function () {
+  $mobileMenu.removeClass("on");
+  $("body").css("overflow", "auto"); // 스크롤 복구
+});
+
+// 모바일 서브메뉴 토글
+$mobileGnb.on("click", function (e) {
+  e.preventDefault();
+  $(this).next(".mobile-submenu").slideToggle(300);
+  $(this).parent().siblings().find(".mobile-submenu").slideUp(300);
+});
+
+// 모바일 메뉴 외부 영역 클릭시 닫기
+$(document).on("click", function (e) {
+  if (!$(e.target).closest(".mobile-menu, .btn-menu").length) {
+    $mobileMenu.removeClass("on");
+    $("body").css("overflow", "auto");
+  }
+});
+
+// foot family 기능
+if ($(".btn-family").length && $(".family-list").length) {
+  const family = $(".btn-family");
+  const familyList = $(".family-list");
+  const duration = 300;
+
+  family.on("click", function () {
+    familyList.toggleClass("on");
+    familyList.slideToggle(duration);
+  });
+}
+
+// 비주얼 이미지 나타나기~
+gsap.registerPlugin(ScrollTrigger);
+
+const mainPic = $(".main-pic");
+const mainTitle = $(".main-title");
+const mainTl = gsap.timeline({
+  defaults: { duration: 1, ease: "power4.inOut" },
+});
+
+mainTl.from(mainPic, { scale: 0.3 });
+mainTl.from(mainTitle, { y: 200, autoAlpha: 0 }, "-=0.3");
+
+// 1. visual 영역 애니메이션
+const visualPic = $(".visual-pic").get(0);
+
+const visualTl = gsap.timeline({
+  defaults: { duration: 1, ease: "power4.inOut" },
+});
+visualTl.from(visualPic, { scale: 3, filter: "blur(30px)", duration: 2 });
+
+visualTl.from(".bread", { y: 50, autoAlpha: 0 }, "-=0.9");
+visualTl.from(".visual-title h2", { y: 100, autoAlpha: 0 }, "-=0.6");
+visualTl.from(".visual-title p", { y: 100, autoAlpha: 0 }, "-=0.6");
+
+/* MAIN!!!---------------------------------------------- */
+
+// menu swiper(MAIN)
+if ($(".menu-con-slider").length) {
+  const $menuConSlider = new Swiper(".menu-con-slider", {
+    loop: true,
+    slidesPerView: "auto",
+    freeMode: true,
+    watchSlidesProgress: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+  });
+
+  const $menuTxtSlider = new Swiper(".menu-txt-slider", {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+      delay: 5000,
+    },
+    thumbs: {
+      swiper: $menuConSlider,
+    },
+  });
+}
+
+if ($(".news-swiper").length) {
+  const $eventSwiper = new Swiper(".news-swiper", {
+    loop: true,
+    slidesPerView: "1",
+    spaceBetween: 20,
+    autoplay: {
+      delay: 1000,
+    },
+
+    breakpoints: {
+      1300: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+      800: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      600: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      375: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+  });
+}
+
+/* MENU!!!---------------------------------------------- */
+
+if ($(".menu-list li")) {
+  const $menuList = $(".menu-list");
+  $menuList.on("click", function () {
+    $(this).toggleClass("on", 400);
+  });
+}
+
+const $menuItem = $(".menu-list li");
+const $menuList = $(".menu-list");
+
+$menuItem.on("click", function () {
+  $(menuList).toggleClass("on");
+
+  $(menuList).siblings().find($menuItem).stop().slideUp(duration);
+
+  // $(this).find($answer).slideDown(duration);
+  // 선택한 놈의 자손중 답변을 찾아서 슬라이드 토글
+  $(menuList).find($menuItem).stop().slideToggle(duration);
+});
+
+/* REWARDS!!!---------------------------------------------- */
+
+// event swiper(MAIN)
+const $eventTabMenu = $(".event-tab > li");
+const $eventTabCon = $(".event-list");
+
+$eventTabCon.hide();
+$eventTabCon.eq(0).show();
+$eventTabMenu.eq(0).find("a").addClass("on");
+
+$eventTabMenu.on("click", function (e) {
+  e.preventDefault();
+
+  const eventTabIdx = $(this).index();
+
+  $eventTabMenu.find("a").removeClass("on");
+  $(this).find("a").addClass("on");
+
+  $eventTabCon.hide();
+  $eventTabCon.eq(eventTabIdx).show();
+
+  if (eventTabIdx === 0 && window.eventSwiper) {
+    window.eventSwiper.update();
+  }
+});
+
+if ($(".event-swiper").length) {
+  const $eventSwiper = new Swiper(".event-swiper", {
+    loop: true,
+    slidesPerView: "2",
+    spaceBetween: 20,
+    autoplay: {
+      delay: 1000,
+    },
+
+    breakpoints: {
+      1440: {
+        slidesPerView: 7.5,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      425: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+
+    pagination: {
+      el: ".event-list .swiper-pagination",
+      type: "fraction",
+    },
+  });
+}
+
+const $question = $(".info-wrap > ul > li");
+const $answer = $(".answer-wrap");
+
+$question.on("click", function () {
+  // 🚩 $(this)로 구별, siblings()
+
+  // 선택한 놈을 기준으로, 다른 놈들은 on클래스 삭제
+  $(this).siblings().removeClass("on");
+
+  // $(this).addClass("on");
+  // 선택한 놈을 기준으로 on클래스를 토글
+  $(this).toggleClass("on");
+
+  // 선택한 놈의 형제, 하위에 있는 답변은 올리고
+  // stop()  <-- 여러개 예약되어 있어도 한 번만 작동
+  $(this).siblings().find($answer).stop().slideUp(duration);
+
+  // $(this).find($answer).slideDown(duration);
+  // 선택한 놈의 자손중 답변을 찾아서 슬라이드 토글
+  $(this).find($answer).stop().slideToggle(duration);
+});
+
+/* BUSINESS!!!---------------------------------------------- */
+if ($(".allergie-slider").length) {
+  const allergieSwiper = new Swiper(".allergie-slider", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".allergie-slider-wrap .swiper-pagination",
+      type: "fraction",
+    },
+
+    breakpoints: {
+      1440: {
+        slidesPerView: 8,
+        spaceBetween: 20,
+      },
+      830: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+      700: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+}
+
+/* MAP!!!---------------------------------------------- */
+
+const $mapSearch = $(".map-search");
+const $btnFold = $(".btn-fold");
+
+// 페이지 로드 시 초기 상태 설정
+$(document).ready(function () {
+  if (window.innerWidth <= 1024) {
+    $mapSearch.addClass("on");
+  }
+});
+
+// 리사이즈 이벤트 처리
+$(window).on("resize", function () {
+  if (window.innerWidth <= 1024) {
+    $mapSearch.addClass("on");
+  } else {
+    $mapSearch.removeClass("on");
+  }
+});
+
+$btnFold.on("click", function () {
+  $mapSearch.toggleClass("on");
+});
+
+// TOP 버튼
+AOS.init();
+
+const btnTop = document.querySelector(".btn-top");
+const btnTalk = document.querySelector(".btn-talk");
+const html = document.documentElement;
+const htmlPos = html.scrollHeight / 2;
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY;
+
+  if (scrollTop >= htmlPos) {
+    btnTop.classList.add("active");
+    btnTalk.classList.add("active");
+  } else {
+    btnTop.classList.remove("active");
+    btnTalk.classList.remove("active");
+  }
+});
